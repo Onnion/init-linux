172b33e479014d02a769bcb96ffb0e97428a31c3
Marcio Henrique <marcio.hsdl@gmail.com>
2021-02-28 22:09:47 -0300

    feat(node): Add node section

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
 # APACHE
 apt update
 sudo apt install apache2
 sudo ufw allow 'Apache'
 # END APACHE
+
+# NODE
+apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm isntall --lts
+
+n=$(which node); \
+n=${n%/bin/node}; \
+chmod -R 755 $n/bin/*; \
+sudo cp -r $n/{bin,lib,share} /usr/local
+# END NODE
